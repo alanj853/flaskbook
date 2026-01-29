1ca4d4e8c54d5a9b5d49a5eed5c5e41348fcc8a9
alanj853 <alanjackson.nuig@gmail.com>
2026-01-29 20:39:25 +0000

    Add flask_login

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -7,5 +8,12 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+## create and initialise LoginManager
+login = LoginManager(app)
+
+## "login" is the view. You could also pass this to url_for
+login.login_view = 'login'
+
 
 from app import routes, models
