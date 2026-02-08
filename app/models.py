58bb7f8ed5165fb58ada893b5781f1d71b6760eb
alanj853 <alanjackson.nuig@gmail.com>
2026-02-08 18:03:10 +0000

    Add followers table

--- app/models.py
+++ app/models.py
@@ -7,6 +7,15 @@ import sqlalchemy.orm as so
 from app import db, login
 from hashlib import md5
 
+followers = sa.Table(
+    'followers',
+    db.metadata,
+    sa.Column('follower_id', sa.Integer, sa.ForeignKey('user.id'),
+              primary_key=True),
+    sa.Column('followed_id', sa.Integer, sa.ForeignKey('user.id'),
+              primary_key=True)
+)
+
 @login.user_loader
 def load_user(id):
     return db.session.get(User, int(id))
@@ -26,6 +35,15 @@ class User(UserMixin, db.Model):
 
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
         default=lambda: datetime.now(timezone.utc))
+    
+    following: so.WriteOnlyMapped['User'] = so.relationship(
+        secondary=followers, primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        back_populates='followers')
+    followers: so.WriteOnlyMapped['User'] = so.relationship(
+        secondary=followers, primaryjoin=(followers.c.followed_id == id),
+        secondaryjoin=(followers.c.follower_id == id),
+        back_populates='following')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
